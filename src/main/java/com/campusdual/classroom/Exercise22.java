ada5d9237ec3f9074f23e1dc6537063e2b4e3a93
IagoOlivares <iago2002oc@gmail.com>
2024-11-22 09:07:21 +0100

    Ejercicio 22 completado

--- src/main/java/com/campusdual/classroom/Exercise22.java
+++ src/main/java/com/campusdual/classroom/Exercise22.java
@@ -7,19 +7,44 @@ import java.util.Set;
 public class Exercise22 {
 
     public static List<String> createArrayList() {
+        List<String> list = new ArrayList<>();
+        pruebaLista(list);
+        return list;
+    }
 
+    private static void pruebaLista(List<String> customList) {
+        customList.add("GKFFD");
+        customList.add("TNANA");
+        customList.add("MPMSL");
+        customList.add("PSWME");
+        customList.add("LZMLF");
+        customList.add("JYEBV");
+        customList.add("YELNT");
+        customList.add("JSNKR");
+        customList.add("JFESF");
+        customList.add("TMJLL");
     }
 
     public static void printElementsAndIndex(List<String> customList) {
-
+        for (String element : customList ) {
+            System.out.println(element);
+        }
     }
 
     public static boolean addElementToList(List<String> customList, String element) {
-
+        return customList.add(element);
     }
 
     public static void main(String[] args) {
+        List<String> lista = createArrayList();
 
+        System.out.println("Lista de elementos: ");
+        printElementsAndIndex(lista);
+
+        addElementToList(lista, "AAAAA");
+
+        System.out.println("\nLista con el nuevo elemento: ");
+        printElementsAndIndex(lista);
     }
 
 
